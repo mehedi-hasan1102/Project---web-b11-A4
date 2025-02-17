cf30118c2489fff571481f8f9830e24c30c8be6e
hasancodex <dev.hasan10@gmail.com>
2025-02-17 18:24:01 +0600

    cashout

--- cashout.js
+++ cashout.js
@@ -1,14 +1,14 @@
 function cashOut(money) {
-    // চেক করব ইনপুট কি নাম্বার না
+    
     if (typeof money !== "number" || money < 0) {
         return "Invalid";
     }
 
-    // ক্যাশ আউট চার্জ হিসাব করা (1.75%)
+    
     return money * 1.75 / 100;
 }
 
-// ✅ টেস্ট রান:
+
 console.log(cashOut(2000));  // 35
 console.log(cashOut(100));   // 1.75
 console.log(cashOut(999));   // 17.4825
